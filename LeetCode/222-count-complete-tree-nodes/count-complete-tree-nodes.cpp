803fecb74c2b7ce8df6c523aa18f094327416059
Arya Gupta <141574060+AryaGupta01@users.noreply.github.com>
2025-03-13 23:25:56 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.59%) - LeetSync

--- LeetCode/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ LeetCode/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -13,8 +13,6 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
-        int x= countNodes(root->left);
-        int y= countNodes(root->right);
-        return x+y+1;
+        return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
